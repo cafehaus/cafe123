fe621386f0e367fc18645623d94ae95fb95ddc3b
ZHOU <510878689@qq.com>
2022-11-21 23:32:13 +0800

    feat:优化配置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,6 +5,8 @@ module.exports = {
   description: '咖啡教室主页，分享跟咖啡相关的一切',
   // base: '/cafe123/',
   sirDir: 'docs/src',
+  lastUpdated: true,
+  // lastUpdatedText: '最近更新',
   themeConfig: {
     nav: [
       { text: '微博', link: 'https://weibo.com/u/3503148914' },
@@ -12,5 +14,14 @@ module.exports = {
       { text: 'B站', link: 'https://space.bilibili.com/25400077' },
     ],
     sidebar,
+    docFooter: {
+      prev: '上一篇',
+      next: '下一篇'
+    },
+    lastUpdatedText: '最近更新',
+    footer: {
+      message: 'Hello coffee.',
+      copyright: 'Copyright © 2022 cafehaus'
+    }
   },
 }
