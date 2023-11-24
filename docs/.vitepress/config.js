03cfc19f56c79edaa0b944686595d6c1e3ce1df0
ZHOU <510878689@qq.com>
2023-11-24 12:22:33 +0800

    feat:更新站点信息

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,16 +2,18 @@ const sidebar = require('./sidebar')
 
 module.exports = {
   title: '咖啡教室',
-  description: '咖啡教室主页，分享跟咖啡相关的一切',
-  // base: '/cafe123/',
+  titleTemplate: '分享跟咖啡相关的一切',
+  description: '咖啡教室主页，咖啡教程、咖啡拉花、咖啡资讯、咖啡文化、咖啡馆、咖啡师、手冲咖啡、鸡尾酒、饮品...在咖啡教室，有关于咖啡的各种内容',
+  head: [['link', { rel: 'icon', href: '/logo.svg' }]],
   sirDir: 'docs/src',
   lastUpdated: true,
-  // lastUpdatedText: '最近更新',
   themeConfig: {
+    logo: '/logo.svg',
     nav: [
       { text: '微博', link: 'https://weibo.com/u/3503148914' },
       { text: '知乎', link: 'https://www.zhihu.com/people/cafehaus' },
       { text: 'B站', link: 'https://space.bilibili.com/25400077' },
+      { text: '公众号', link: 'https://mp.weixin.qq.com/s/VlgEKvGjd70VNcog0x-slw' },
     ],
     sidebar,
     docFooter: {
--- docs/index.md
+++ docs/index.md
@@ -6,13 +6,24 @@ hero:
   # text: 咖啡教室主页
   tagline: 就想开家小小咖啡馆，做做咖啡、调调酒...
   image:
-    src: /logo.svg
+    src: /miniapp.jpg
     alt: 咖啡教室
   actions:
     - theme: brand
       text: 咖啡词典
-      link: /src/咖啡词典/咖啡豆/index
+      link: /src/咖啡词典/咖啡产地名/index
     - theme: alt
       text: 咖啡风味轮
       link: /src/咖啡资源/咖啡风味轮/index
+features:
+  - icon: 🍉
+    title: 咖啡网站导航
+    details: 全球知名咖啡博客、咖啡组织、咖啡馆、咖啡设计、咖啡师...网址导航，第一时间了解咖啡最新资讯
+    link: /src/咖啡资源/咖啡网站导航/index
+    linkText: 去瞅瞅
+  # - icon: 👨‍🏫
+  #   title: 咖啡知识库
+  #   details: 咖啡教程、咖啡拉花、咖啡文化、咖啡馆、咖啡师、咖啡豆、咖啡烘焙、手冲咖啡...关于咖啡的各种知识
+  #   link: /src/咖啡资源/咖啡网站导航/index
+  #   linkText: 去瞅瞅
 ---
